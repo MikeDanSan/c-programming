1f7fb1afd5790dcdd22bd339ee53c144a2c08153
mike <mikedansanchez@gmail.com>
2024-11-08 02:58:10 +0000

    basic: print and compute with integers

--- basics/compute_integers/compute_integers.c
+++ basics/compute_integers/compute_integers.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 
+
+
 /*
-    Notes:
+ * @file compute_integers.c
+ * @brief This file contains functions for performing basic integer computations.
+ * 
+ * This program is part of the "basics" module in the C programming series.
+ * It includes functions to compute various operations on integers.
+ * 
+ * @author Mike
+ * @date 2023
+ */
 
-    - The program will compute the sum of the integers from 1 to 10.
-
-*/
 
 int main(void) {
 
 
+    printf("Dear Procrastinator,\nYou still have to wait for %d days (%d minutes or %d seconds) before you can procrastinate!", 25-23, 60*24*(25-23), 60*60*24*(25-23));
 
     return 0;
 }
