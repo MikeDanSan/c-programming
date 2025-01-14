e7c0f7fee911d445a7aa0e26bca53ceb27d8c697
mike <mikedansanchez@gmail.com>
2025-01-14 01:31:18 +0000

    lesson: update passing arrays to functions.

--- intermediate/pointers/passing_array_functions/main.c
+++ intermediate/pointers/passing_array_functions/main.c
@@ -1,29 +1,42 @@
 #include <stdio.h>
 
-void reverseArray(int *);
+void behind(int *, int);
 
-int main() {
-    // populate an 6 element array from input
-    int arr[6];
-    for (int i = 0; i < 6; i++) {
-        scanf("%d", &arr[i]);
+int main(void) {
+    int array[10];
+    int N, i;
+    
+    scanf("%d", &N);
+    for (i=0; i<N; i++) {
+        scanf("%d", &array[i]);
     }
-
-    // call reverseArray function
-    reverseArray(arr);
-
-    // print the reversed array
-    for (int i = 0; i < 6; i++) {
-        printf("%d ", arr[i]);
-    }
-}
-
-void reverseArray(int * arr) {
-    // reverse the array
-    for (int i = 0; i < 3; i++) {
-        int temp = arr[i];
-        arr[i] = arr[5 - i];
-        arr[5 - i] = temp;
+    behind(array, N);
+    for (i=0; i<N; i++) {
+        printf("%d\n", array[i]);
     }
     
+    return 0;
 }
+
+/* Write your function behind() here: */
+void behind(int * array, int size) {
+
+    int result;
+
+    // find largest int in array
+    int largest = array[0];
+
+    for (int i = 1; i < size; i++) {
+        if (array[i] > largest) {
+            largest = array[i];
+        }
+    }
+
+    // subtract largest int from all ints in array output the result with newline
+    for (int i = 0; i < size; i++) {
+        result = largest - array[i];
+        array[i] = result;
+    }
+
+}
+
